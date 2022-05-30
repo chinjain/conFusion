00f109e92f68b76be343a7316ec9b154bdc60974
chinmay Jain <chinmayjain03@gmail.com>
2022-05-30 17:29:11 +0530

    component menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path:'',component:AppComponent},
+  // {
+  //   path:'/menu',component:MenuComponent
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
