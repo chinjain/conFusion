1c740f655a8e0892577370a7d90565ac78525317
chinmay Jain <chinmayjain03@gmail.com>
2022-06-01 11:52:00 +0530

    Detail Controller Fixed

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,10 +1,13 @@
-export interface Dish{
-    id:string;
-   name:string; 
-   image:string;
-   category:string;
-   featured:boolean;
-   label:string;
-   price:string;
-   description:string;
+import { Comment } from "./Comment";
+
+export class Dish {
+    id: string;
+    name: string;
+    image: string;
+    category: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    description: string;
+    comments: Comment[];
 }
