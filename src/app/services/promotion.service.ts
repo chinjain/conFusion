2d261f277777f35586dc1efffbb76e72a3db5e2d
chinmay Jain <chinmayjain03@gmail.com>
2022-06-02 13:48:43 +0530

    Promotion Services Added

---
+++ src/app/services/promotion.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Promotion } from '../shared/promotion';
+import { PROMOTIONS } from '../shared/promotions';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PromotionService {
+
+  constructor() { }
+  
+  getPromotions():Promotion[]{
+    return PROMOTIONS;
+  }
+}
